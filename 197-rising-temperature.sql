b4428d6f9c118d613cf6c485e9f9d2143de6918f
Deeksha Singhai <132802902+deekshasinghai@users.noreply.github.com>
2024-05-29 12:30:31 +0530

    Create 197-rising-temperature.sql

---
+++ 197-rising-temperature.sql
@@ -0,0 +1,8 @@
+--Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+--Return the result table in any order.
+
+Query:
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+
